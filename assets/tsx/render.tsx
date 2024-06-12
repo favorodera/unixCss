9e4ef9fe29545817377349a791e329ffc133db5a
Favour Chidera Emeka <favorodera@gmail.com>
2024-06-12 12:56:33 +0100

    update

--- assets/tsx/render.tsx
+++ assets/tsx/render.tsx
@@ -1,6 +1,11 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import Index from ".";
 import "../scss/index.scss";
 
 const app = ReactDOM.createRoot(document.querySelector(".root-container")!);
-app.render(<Index />);
+app.render(
+  <React.StrictMode>
+    <Index />
+  </React.StrictMode>
+);
